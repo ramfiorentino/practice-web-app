043fac31d7428a93cc085580f48c7390a2bfe5a0
Ram Fiorentino <mariabf.bcn@gmail.com>
2024-09-26 07:46:02 +0200

    Add js validation for name input

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 // Hi! This is your root js file, where the logic comes together to make great changes!
 
-document.getElementById("name").addEventListener("change", nameValidation);
+// listening to SUBMIT BUTTON
+document.getElementById("registerForm").addEventListener("submit", handleSubmit);
+
+//Passing event to the callback for safe preventDefault
+function handleSubmit(event) {
+    nameValidation(event);
+    event.preventDefault();
+  }
+
+// listening to NAME
+//document.getElementById("name").addEventListener("change", nameValidation);
+
 
 function nameValidation() {
-  console.log("Im name validating");
-  document.getElementById("div-alert").style.backgroundColor = "red";
-  document.getElementById("div-alert").innerHTML = "THIS IS SO WRONG";
+  console.log("Function nameValidations is being called");
+
+  // turning the user name string into an array, separating items wherever there is a blank space
+  const userName = document.getElementById("name").value.split(" ");
+  console.log("Split user name " + userName);
+
+  if (userName.length >= 2) {
+    console.log("Correct: The name has at least two words.");
+  } 
+  else {
+    const alertMessage = "Wrong, the name has at least two words.";
+    console.log(alertMessage);
+    document.getElementById("name").style.border = "3px solid red";
+    document.getElementById("name-error").innerHTML = "Wrong, the name has at least two words.";
+    document.getElementById("name-error").hidden = false;
+  };
+
 }
